4029ef3860e984a7d1e0c3a4a1e22dd63c6d9fd3
Mac <mac@Korn-MacBook-Pro.local>
2020-09-02 04:43:49 +0700

    commit on master

--- bot_setting.php
+++ bot_setting.php
@@ -2,5 +2,5 @@
 # setting LINE Messageing API
 define('LINE_MESSAGE_CHANNEL_ID','1654880290'); 				// Channel ID
 define('LINE_MESSAGE_CHANNEL_SECRET','11244a6442f44298bc083b311d05e82f'); 	// Channel secret
-define('LINE_MESSAGE_ACCESS_TOKEN','8IFoohQjh502i+Ch+2+C8+73GuKwMneh/H+CYe+iENi2qdLnv3/wn4c8H3o7irFc0bFhaNuK7JQCC1Zq6vczjMqfR2nddoIVTVi9unF5j6a2pZFwv3bzwxeGAxRctptuANsiZJ1Dgd+ia5oWarsJugdB04t89/1O/w1cDnyilFU='); // Channel access token
+define('LINE_MESSAGE_ACCESS_TOKEN','h1e1F38wPPjkZq+1/xJWrZ1hhTdf0aH85fnVfyLFLjdzPDDj4ZvkNcQCltwFNu7e0bFhaNuK7JQCC1Zq6vczjMqfR2nddoIVTVi9unF5j6ZRZC2vdYirOkAUJcLR4cnjRqs+K9xDPp7GMt8au3/ReAdB04t89/1O/w1cDnyilFU='); // Channel access token
 ?>
